3ceb30051859dd516aa94fc1534de5bbcdd69fe0
stephenblackwood <hotpants33067@gmail.com>
2022-07-05 12:23:19 -0500

    Initial commit

---
+++ src/main/java/Fibonacci.java
@@ -0,0 +1,27 @@
+package org.example;
+
+/**
+ * Fibonacci class used for findinng the corresponding fibonacci num
+ *
+ * @author Created by stephen Blackwood on 05/20/2022
+ */
+public class Fibonacci {
+    /**
+     * @param n is going to be any int the user inputs
+     * @return going to return a long because the numbers tend to grow at a very fast rate
+     */
+    static long fibonacci(int n) {
+        //base case (to stop infinite calls)
+        if (n == 0)
+            return 0;
+        //base case(to stop infinite calls )
+        if (n == 1)
+            return 1;
+
+        return (fibonacci(n - 1) + fibonacci(n - 2));
+
+
+    }
+
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter number: ");
+        int a = scan.nextInt();
+        int n = a;
+        System.out.print(Fibonacci.fibonacci(n)+"  is the  "+ (a) + "  term of the Fibonacci sequence ");
+    }
+
+
+
+////Users/stephenblackwood/IdeaProjects/fib/src/com/company
+
+}
+
+
+
+
